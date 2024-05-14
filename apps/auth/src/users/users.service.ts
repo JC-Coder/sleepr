fe3af54b5f4a1dcff26a0efc604052783120ca42
JC-Coder <josephchimezie2003@gmail.com>
2024-05-14 10:17:56 +0100

    add jwt auth

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,5 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from 'apps/auth/src/users/dto/create-user.dto';
+import { GetUserDto } from 'apps/auth/src/users/dto/get-user.dto';
 import { UsersRepository } from 'apps/auth/src/users/users.repository';
 import * as bcrypt from 'bcryptjs';
 
@@ -8,10 +13,20 @@ export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
+    await this.validateCreateUserDto(createUserDto);
     return await this.userRepository.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
     });
+  }
+
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.userRepository.findOne({ email: createUserDto.email });
+    } catch (error) {
+      return;
+    }
+    throw new UnprocessableEntityException('Email already exists');
   }
 
   async verifyUser(email: string, password: string) {
@@ -24,5 +39,9 @@ export class UsersService {
     }
 
     return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return await this.userRepository.findOne(getUserDto);
   }
 }
---
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,24 @@
+import { AUTH_SERVICE } from '@app/common/constants/services';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
+
+    if (!jwt) {
+      return false;
+    }
+  }
+}
